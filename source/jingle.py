258076c228e2bcbfa9cf4ae9a6dd598a8ddba0ab
Millo Brennan-Evers <exedysdigital@gmail.com>
2019-12-20 19:54:22 +0000

    Add files via upload

---
+++ source/jingle.py
@@ -0,0 +1,16 @@
+## Jingle Language v0.01
+
+import sys
+from antlr4 import *
+from parse.JingleLexer import JingleLexer
+from parse.JingleParser import JingleParser
+ 
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = JingleLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = JingleParser(stream)
+    tree = parser.topLevelDecl()
+ 
+if __name__ == '__main__':
+    main(sys.argv)
