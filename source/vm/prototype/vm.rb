84c3640a4489b7680cf83f5b94ccc740b0da593c
Millo Brennan-Evers <exedysdigital@gmail.com>
2020-01-10 23:18:40 +0000

    Create vm.rb

---
+++ source/vm/prototype/vm.rb
@@ -0,0 +1,42 @@
+require "bytecode"
+
+class VM
+  def run(bytecode)
+    stack = []
+    ip = 0
+
+    while true
+      case bytecode[ip] # Inspect the current byte, this will be the opcode.
+
+      when PUSH_NUMBER
+        ip += 1 
+        value = bytecode[ip] 
+        
+        stack.push Constants["Number"].new_with_value(value)
+
+      when PUSH_SELF
+        stack.push RootContext.current_self
+
+      when CALL
+        ip += 1 
+        method = bytecode[ip]
+        
+        ip += 1 
+        argc = bytecode[ip]
+
+        args = []
+        argc.times do
+          args << stack.pop
+        end
+        receiver = stack.pop
+
+        stack.push receiver.call(method, args)
+        
+        return stack.pop
+
+      end
+      
+      ip += 1
+    end
+  end
+end
