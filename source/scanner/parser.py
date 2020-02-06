b969633eae9a2a03bb5a6181c97b42779abbd2f9
Millo Evers <exedysdigital@gmail.com>
2020-02-06 21:59:27 +0000

    Add files via upload

--- source/scanner/parser.py
+++ source/scanner/parser.py
@@ -1,8 +1,17 @@
 # Jingle Parser
+import time
+start_time = time.time()
 
-from scanner import Lark_StandAlone
-parser = Lark_StandAlone()
-input = open("testfile.jn", "r")
-input = input.read()
-tree = parser.parse(input)
-print(tree.pretty())
+def parseScan():
+    from scanner import Lark_StandAlone
+    parser = Lark_StandAlone()
+    input = open("testfile.jn", "r")
+    input = input.read()
+    tree = parser.parse(input)
+    print(tree.pretty())
+
+parseScan()
+
+final_time = time.time() - start_time
+final_time = round(final_time, 3)
+print("--- %s seconds ---" % (final_time))
