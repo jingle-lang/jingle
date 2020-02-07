35e5de66f98ee74d9864dbcb3d96f16154f40274
Millo Evers <exedysdigital@gmail.com>
2020-02-07 17:53:34 +0000

    Add files via upload

--- source/scanner/parser.py
+++ source/scanner/parser.py
@@ -9,9 +9,18 @@ def parseScan():
     input = input.read()
     tree = parser.parse(input)
     print(tree.pretty())
+    final_time = time.time() - start_time
+    final_time = round(final_time, 3)
+    print("--- Parsing complete after %s seconds ---" % (final_time))
 
+# Singular Parse Testing
 parseScan()
 
-final_time = time.time() - start_time
-final_time = round(final_time, 3)
-print("--- %s seconds ---" % (final_time))
+# Repeated parse testing
+#parseCounts = 793
+#while parseCounts > 0:
+#    parseScan()
+#    parseCounts = parseCounts - 1
+
+#while True:
+#    parseScan()
