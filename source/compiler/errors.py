8fcad9b16ab3adc67d4b87ab90ae3f658a8e942f
Millo Evers <exedysdigital@gmail.com>
2020-04-10 19:52:05 +0100

    Update errors.py

--- source/compiler/errors.py
+++ source/compiler/errors.py
@@ -1,29 +1,4 @@
-# gone/errors.py
-'''
-Compiler error handling support.
-
-One of the most important (and annoying) parts of writing a compiler
-is reliable reporting of error messages back to the user.  This file
-defines some generic functionality for dealing with errors throughout
-the compiler project.  You might want to expand this with additional
-capabilities for the purposes of unit testing.
-
-To report errors in your compiler, use the error() function. For example:
-
-       error(lineno, 'Some kind of compiler error message')
-
-where lineno is the line number on which the error occurred.   If your
-compiler supports multiple source files, add the filename keyword argument.
-
-       error(lineno, 'Some kind of error message', filename='foo.src')
-
-The utility function errors_reported() returns the total number of
-errors reported so far.  Different stages of the compiler might use
-this to decide whether or not to keep processing or not.
-
-Use clear_errors() to clear the total number of errors.
-'''
-
+# errors.py
 import sys
 
 _num_errors = 0
